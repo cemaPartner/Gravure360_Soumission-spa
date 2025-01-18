3ef36c86e984fefdc3f8fbc0fa18d8b37944203f
CemaPartner <cema.partner@gmail.com>
2025-01-18 13:44:23 -0500

    Page accueil

--- src/app/components/pages/client/welcome/welcome.component.ts
+++ src/app/components/pages/client/welcome/welcome.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../../service/auth.service';
+import { User } from '../../../../model/user';
+import { Subscription } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,
+    NgIf
+  ],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss'
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
+  isConnected: boolean = false;
+  currentUser: User = new User;
+  private subscription: Subscription = new Subscription;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.subscription = this.authService.activeUserObservable.subscribe((value) => {
+      this.isConnected = value;
+      this.currentUser = this.authService.currentUser;
+    });
+  }
 
 }
